c56434cb379cc9eae41b255ae51cd018f1dad237
SABARISH V <vivohari91@gmail.com>
2026-07-24 14:38:32 +0530

    Add files via upload

---
+++ day6_Data Visualizatio.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("clean_student_scores.csv")
+
+# Scatter Plot
+plt.scatter(data["Hours"], data["Score"])
+plt.title("Scatter Plot")
+plt.xlabel("Study Hours")
+plt.ylabel("Exam Score")
+plt.show()
+
+# Line Chart
+plt.plot(data["Hours"], data["Score"])
+plt.title("Line Chart")
+plt.xlabel("Study Hours")
+plt.ylabel("Exam Score")
+plt.show()
+
+# Bar Chart
+plt.bar(data["Hours"], data["Score"])
+plt.title("Bar Chart")
+plt.xlabel("Study Hours")
+plt.ylabel("Exam Score")
+plt.show()
