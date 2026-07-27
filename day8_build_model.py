64772eb74df1992d3a9070953573bd45b8013ccc
SABARISH V <vivohari91@gmail.com>
2026-07-27 11:18:24 +0530

    Add files via upload

---
+++ day8_build_model.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+data = pd.read_csv("clean_student_scores.csv")
+
+print(data.head())
+# Input (Feature)
+X = data[["Hours"]]
+
+# Output (Target)
+y = data["Score"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print("Model trained successfully!")
+print("Slope (Coefficient):", model.coef_[0])
+print("Intercept:", model.intercept_)
