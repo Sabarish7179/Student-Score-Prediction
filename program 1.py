4d6ff823758dec11eca515615b735391762a6ec2
SABARISH V <vivohari91@gmail.com>
2026-07-31 11:26:18 +0530

    Update program 1.py

--- program 1.py
+++ program 1.py
@@ -5,3 +5,11 @@ if num % 2 == 0:
     print("even")
 else:
     print("odd")
+
+#factorial
+num= int (input("enter a number: "))
+fact=1
+for i in range (1, num+1):
+    fact*=i
+print("factorial=", fact)
+
